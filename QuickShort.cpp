616c40c726cc7ac3c116a1241f75b16c28840ce0
naufaalkh <naufaalkh@gmail.com>
2024-03-28 14:58:27 +0700

    Menambahkan fungsi Input

--- QuickShort.cpp
+++ QuickShort.cpp
@@ -4,3 +4,26 @@ using namespace std;
 int arr[20];       // Array of integers to hold values
 int cmp_count = 0; // Number of comparisons
 int mov_count = 0; // Number of data movements
+
+void input()
+{
+    int n;
+    // Input array length
+    while (true)
+    {
+        cout << "Masukkan panjang elemen array: ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMaksimum panjang array adalah 20" << endl;
+    }
+
+    cout << "\nEnter Array Element" << endl;
+    // Input array elements
+    for (int i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
+}
