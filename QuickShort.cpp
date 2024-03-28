fd6c1db9164c6a47b34d06bc4fbe84c7bfbecbc1
naufaalkh <naufaalkh@gmail.com>
2024-03-28 14:57:58 +0700

    Menambahkan variabel global

--- QuickShort.cpp
+++ QuickShort.cpp
@@ -1,3 +1,6 @@
 #include <iostream>
 using namespace std;
 
+int arr[20];       // Array of integers to hold values
+int cmp_count = 0; // Number of comparisons
+int mov_count = 0; // Number of data movements
